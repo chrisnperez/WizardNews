4b529fc45f481390586627ba0554cd2082143a45
Chris Perez <chrisnaperez@gmail.com>
2023-05-03 20:26:38 -0700

    added title & name to /listings

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const express = require("express");
 const app = express();
+const morgan = require('morgan');
+const postBank = require("./postBank");
 
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.use(morgan('dev'));
+
+
+app.get("/listings", (_req, res) => {
+  const listings = postBank.list();
+
+  const html = `<h1>Comments</h1>
+  <ul>
+  ${listings.map(list => `<li> <strong>${list.name}:</strong> ${list.title}</li>`).join(' ')}
+  </ul>`;
+
+  res.send(html)
+});
 
 const PORT = 1337;
 
